3686be78f8d6ddb5f299e0dcbc7ba6a4bdd1fcdd
MoreiraSte <casagrandestephany91@gmail.com>
2023-05-10 11:28:29 -0300

    success admin auth

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -3,40 +3,52 @@ import React, {useState,useEffect} from 'react';
 import { View, Text, TouchableOpacity,Button,TextInput ,Link} from 'react-native';
 import image_logo from '../images/icon_logo.png'
 import {MaterialCommunityIcons} from "@expo/vector-icons"
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-
-import firebase from "../config/firebase.js"
+import { auth,logInWithEmailAndPassword,signOut  } from '../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Login({navigation}) {
-  const database = firebase.firestore();
+  // const database = firebase.firestore();
 
-  const [email,setEmail] = useState("")
-  const [senha,setSenha] = useState("")
-  const [errorLogin, setErrorLogin] = useState("")
+//   const auth = getAuth();
+// signOut(auth).then(() => {
+//   // Sign-out successful.
+// }).catch((error) => {
+//   // An error happened.
+// });
 
-  const loginFirebase = () =>{
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, email, senha)
-      .then((userCredential) => {
-        let user = userCredential.user;
-        
-        if (email == 'admin@gmail.com' && senha == 'admin123'){
-          this.props.navigation.navigate('Choice')
+  const[email,setEmail] = useState('')
+  const[password,setPassword] = useState('')
+  const[user,loading] = useAuthState(auth)
+  const[errorLogin,setErrorLogin] = useState('')
+
+  // const navigate = useNavigation()
+
+  // quando a pagina recarregar, se estiver em loading vai retornar o que tem de acordo com a configuarção do firebase no arquivo js, se 
+  // reconhecer o usuario, irá navegar até a pagina create.
+  useEffect(() => {
+      if(loading){
+          return;
+      }
+      if(user) {
+        console.log(user)
+        if (user.uid === '2UVpAWuuCEPKTqBstxcdHlRwSEz1'){
+          navigation.navigate('Choice')
         }
-
-   
-      })
-      .catch((error) => {
-        setErrorLogin(true)
-        let errorCode = error.code;
-        let errorMessage = error.message;
+        else{
+          navigation.navigate('Home')
+        }
+        
+      }
+  },[user,loading])
+   // criando uma constante para guardar a função que possui o registro que é o email e password
+  const loginFire = () => {
+      console.log("chamou a função")
+      logInWithEmailAndPassword(email,password).then((res) => {
+        console.log(res)
       });
   }
 
-  useEffect(()=>{
-
-  },[]);
     return (
         <>
       <View style={styles.container}>
@@ -61,8 +73,8 @@ export default function Login({navigation}) {
                   <TextInput
                     style={styles.input}
                     secureTextEntry={true}
-                    onChangeText={(text) => setSenha(text)}
-                    value={senha}
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
                     placeholder="Password"
                     keyboardType="text"
                     type = 'text'
@@ -81,11 +93,11 @@ export default function Login({navigation}) {
              :
              <View/>
             }
-            {email === "" || senha === ""
+            {email === "" || password === ""
             ?
             <TouchableOpacity 
             
-            onPress={loginFirebase}
+            onPress={loginFire}
             style={styles.button}
             disabled={true}
         >
@@ -94,9 +106,9 @@ export default function Login({navigation}) {
           :
           <TouchableOpacity 
             
-            onPress={loginFirebase}
+            onPress={loginFire}
             style={styles.button}
-            disabled={true}
+            // disabled={true}
         >
             <Text style={styles.txtButton}>Sign In</Text>
           </TouchableOpacity>
--- pages/config/firebase.js
+++ pages/config/firebase.js
@@ -1,7 +1,22 @@
-import firebase from 'firebase'
-import 'firebase/storage'
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+import {getStorage} from 'firebase/storage'
 
-var firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAPRF1Zsnt5krkBl4C7f0eYP2AJXrFgcHE",
   authDomain: "japanesedelivery-f850b.firebaseapp.com",
   projectId: "japanesedelivery-f850b",
@@ -10,7 +25,52 @@ var firebaseConfig = {
   appId: "1:871949043089:web:b9f86ba45ffaf69ea10630"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app)
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export default firebase
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logout = () => {
+  signOut(auth);
+};
+export {
+  auth,
+  db,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+  storage,
+};
